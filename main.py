c1e65071b1474d49e2bc0c5404b0e9681b69ace2
iammarshallnz <marshallinch@gmail.com>
2025-07-04 11:55:50 +1200

    Working locally

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+
 from pyscript import document
-import timeit
+import time
 import random
-    
+import asyncio
+
 def randomGrid(col, row) -> list:
     """Generate a random grid of integers."""
     return [
@@ -9,7 +11,7 @@ def randomGrid(col, row) -> list:
         for _ in range(row)
     ]
 
-def solution(maximum_product):
+def solution():
     """
     Generate a random grid of integers and find maximun product of 4 intergers in a row, column, or diagonal.
     """
@@ -45,13 +47,37 @@ def solution(maximum_product):
             product = grid[i][j] * grid[i + 1][j - 1] * grid[i + 2][j - 2] * grid[i + 3][j - 3]
             if product > max_product:
                 max_product = product
-    maximum_product = max_product
-    return
+    return max_product, grid
 
-def translate_english(event):
+
+
     
-    
+
+global running
+running = False
+async def update(current_count, wall_clock):
     output_div = document.querySelector("#output")
-    max_product = 0
-    time_taken = timeit.timeit(lambda: solution(max_product), number=1)
-    output_div.innerText = f"{max_product}" + f" in {time_taken:.2f} seconds \n"
+    output_div.innerText = f"Current time through calculations {current_count} seconds, wall clock time: {wall_clock:.2f} seconds"
+
+async def start(event):
+    print("Starting calculations...")
+    global running
+    running = True
+    current_count = 0
+    wall_clock = 0
+    while running:
+        print("Calculating...")
+        
+        start = time.time()
+        solution()
+        wall_clock += time.time() - start
+        current_count += 1
+    
+        await update(current_count, wall_clock)
+        await asyncio.sleep(0.01)
+
+async def stop(event):
+    global running
+    running = False
+
+
