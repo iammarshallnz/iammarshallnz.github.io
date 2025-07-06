9caccd41bd8ac310165d8907b9d8407866d7f213
iammarshallnz <marshallinch@gmail.com>
2025-07-07 11:41:06 +1200

    Some changes to presentation and the timing main.py

--- timer/main.py
+++ timer/main.py
@@ -11,14 +11,12 @@ def randomGrid(col, row) -> list:
         for _ in range(row)
     ]
 
-def solution():
+def solution(grid, col, row) -> tuple:
     """
     Generate a random grid of integers and find maximun product of 4 intergers in a row, column, or diagonal.
     """
-    col = 600
-    row = 600
-    grid = randomGrid(col, row)
-    max_product = 0
+    
+    max_product : int = 0
     # row 
     
     for i in range(row):
@@ -59,24 +57,28 @@ async def update(current_count, wall_clock):
     output_div = document.querySelector("#output")
     output_div.innerText = f"Current time through calculations {current_count} seconds, wall clock time: {wall_clock:.2f} seconds"
 
-async def start(event):
+async def start() -> None:
     print("Starting calculations...")
     global running
     running = True
     current_count = 0
     wall_clock = 0
+    col = 2000
+    row = 2000
+
+    grid = randomGrid(col, row)
     while running:
         print("Calculating...")
         
         start = time.time()
-        solution()
+        solution(grid, col, row)
         wall_clock += time.time() - start
         current_count += 1
     
         await update(current_count, wall_clock)
         await asyncio.sleep(0.01)
 
-async def stop(event):
+async def stop() -> None:
     global running
     running = False
 
